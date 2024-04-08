3e5654646e805a28f5fbd0d09e92bce747267c5d
Sazzad ALI <sazzadatgithub1212@gmail.com>
2024-04-08 22:54:39 +1000

    first commit

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 
-// server component ...
-async function getMovies(type: string) {
-
-}
 
 export default function Home() {
   return (
     <>
-      Hello World...
+      Hello Movies...
     </>
   );
 }
+
--- src/app/movies/popular/page.tsx
+++ src/app/movies/popular/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 
-// server component ...
-async function getMovies(type: string) {
-
-}
 
 export default function Home() {
   return (
     <>
-      Hello World...
+      Hello Popular Movies...
     </>
   );
 }
